0680df7af5fa424c568b9be7f736728beea96d9b
Ralf Lang <lang@b1-systems.de>
2021-07-20 19:40:34 +0000

    Add PSR7 Bridges.

---
+++ src/Request/Psr7Wrapper.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Copyright 2008-2021 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Controller
+ */
+namespace Horde\Controller\Request;
+use \Horde_Controller_Request as H5Request;
+use Psr\Http\Message\ServerRequestInterface;
+use \Horde_Stream_String as StringStream;
+
+/**
+ * Wrap a PSR-7 Request inside a H5 request
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category  Horde
+ * @copyright 2008-2021 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
+ * @package   Controller
+ */
+class Psr7Wrapper implements H5Request
+{
+    private ServerRequestInterface $request;
+
+    public function __construct(ServerRequestInterface $request)
+    {
+        $this->request = $request;
+    }
+    /**
+     */
+    public function getPath()
+    {
+        $this->request->getUri()->getPath();
+    }
+
+    /**
+     */
+    public function getMethod()
+    {
+        $this->request->getMethod();
+    }
+
+    /**
+     */
+    public function getGetVars()
+    {
+        $getVars = [];
+        parse_str($this->request->getUri()->getQuery(), $getVars);
+        return $getVars;
+    }
+
+    /**
+     */
+    public function getFileVars()
+    {
+        return $this->request->getUploadedFiles();
+    }
+
+    /**
+     */
+    public function getServerVars()
+    {
+        return $this->request->getServerParams();
+    }
+
+    /**
+     */
+    public function getPostVars()
+    {
+        return $this->request->getParsedBody();
+    }
+
+    /**
+     */
+    public function getCookieVars()
+    {
+        return $this->request->getCookieParams();
+    }
+
+    /**
+     * Mimic $_REQUEST by merging get, post and cookies
+     */
+    public function getRequestVars()
+    {
+        return array_merge(
+            $this->getGetVars(),
+            $this->getPostVars(),
+            $this->getCookieVars
+        );
+    }
+
+    /**
+     */
+    public function getSessionId()
+    {
+        return 0;
+    }
+
+    /**
+     * The request body if it is not form-encoded
+     * @returns Horde_Stream
+     */
+    public function getRequestBody()
+    {
+        return new StringStream(
+            ['string' => $this->request->getBody()->getContents()]
+        );
+    }    
+}
